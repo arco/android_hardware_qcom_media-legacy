830116e63f03ac1b580b08253e5c5dea1d5355da
Michael Bestas <mikeioannina@gmail.com>
2013-07-26 16:32:13 +0300

    libc2dcolorconvert: add include for kernel headers
    The header msm_kgsl.h is needed.
    
    Change-Id: Ic3ad83a7e80b99d848174fa047843a69a382cc54
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- libc2dcolorconvert/Android.mk
+++ libc2dcolorconvert/Android.mk
@@ -5,15 +5,26 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
         C2DColorConverter.cpp
 
+kernel_includes :=
+
+ifneq ($(TARGET_KERNEL_SOURCE),)
+    kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+endif
+
 LOCAL_C_INCLUDES := \
     $(TOP)/frameworks/av/include/media/stagefright \
     $(TOP)/frameworks/native/include/media/openmax \
-    $(TOP)/hardware/qcom/display-legacy/libcopybit
+    $(TOP)/hardware/qcom/display-legacy/libcopybit \
+    $(kernel_includes)
 
 LOCAL_SHARED_LIBRARIES := liblog libdl
 
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE := libc2dcolorconvert
+
+ifneq ($(TARGET_KERNEL_SOURCE),)
+LOCAL_ADDITIONAL_DEPENDENCIES  := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+endif
 
 include $(BUILD_SHARED_LIBRARY)
